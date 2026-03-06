c748f2d0e5a7fba65f27b7ebaf674b539472b312
= <uday>
2026-03-06 10:09:27 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_user_info():
+    name = input("Enter your name: ")
+    while True:
+        age_input = input("Enter your age: ")
+        if age_input.isdigit():
+            age = int(age_input)
+            break
+        else:
+            print("That doesn't look like a number. Please try again.")
+
+    print("\n--- Profile Created ---")
+    print(f"User: {name}")
+    print(f"Status: {age} years old")
+
+
+get_user_info()
